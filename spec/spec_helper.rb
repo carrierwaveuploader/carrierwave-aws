3005592541f38c620bfdd0b72faddbe77aad6944
Parker Selbert <parker@sorentwo.com>
2013-04-19 10:06:25 -0500

    Add rspec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
 require 'rspec'
 require 'carrierwave'
 require 'carrierwave-aws'
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus
+  config.order = 'random'
+  config.run_all_when_everything_filtered = true
+end
