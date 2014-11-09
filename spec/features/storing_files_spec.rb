86c3f35c6d39083b1d1ad8de573667ed383bb852
Felix Bünemann <buenemann@louis.info>
2014-11-09 19:25:42 +0100

    Allow integration tests to work with v4 regions.
    For newer regions that require v4 signing, specifying the region is
    mandatory or signing will fail.

--- spec/features/storing_files_spec.rb
+++ spec/features/storing_files_spec.rb
@@ -10,7 +10,8 @@ if ENV['S3_BUCKET_NAME']
 
         config.aws_credentials = {
           access_key_id:     ENV['S3_ACCESS_KEY'],
-          secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
+          secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+          region:            ENV['S3_REGION']
         }
       end
     end
