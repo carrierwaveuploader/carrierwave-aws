1325308c22f973fc6c1fa3a84a3370fa1af9faec
Felix Bünemann <buenemann@louis.info>
2014-11-09 19:51:26 +0100

    Extract shared feature spec setup into spec helper

--- spec/features/storing_files_spec.rb
+++ spec/features/storing_files_spec.rb
@@ -1,34 +1,18 @@
 require 'spec_helper'
 
-if ENV['S3_BUCKET_NAME']
-  describe 'Storing Files' do
-    before(:all) do
-      CarrierWave.configure do |config|
-        config.storage    = :aws
-        config.aws_bucket = ENV['S3_BUCKET_NAME']
-        config.aws_acl    = :public_read
-
-        config.aws_credentials = {
-          access_key_id:     ENV['S3_ACCESS_KEY'],
-          secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-          region:            ENV['S3_REGION']
-        }
-      end
+describe 'Storing Files', type: :feature do
+  it 'uploads the file to the configured bucket' do
+    uploader = Class.new(CarrierWave::Uploader::Base) do
+      def filename; 'image.png'; end
     end
 
-    it 'uploads the file to the configured bucket' do
-      uploader = Class.new(CarrierWave::Uploader::Base) do
-        def filename; 'image.png'; end
-      end
+    image    = File.open('spec/fixtures/image.png', 'r')
+    instance = uploader.new
 
-      image    = File.open('spec/fixtures/image.png', 'r')
-      instance = uploader.new
+    instance.store!(image)
+    instance.retrieve_from_store!('image.png')
 
-      instance.store!(image)
-      instance.retrieve_from_store!('image.png')
-
-      expect(instance.file.size).to be_nonzero
-      expect(image.size).to eq(instance.file.size)
-    end
+    expect(instance.file.size).to be_nonzero
+    expect(image.size).to eq(instance.file.size)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,11 +11,14 @@ def source_environment_file!
 end
 
 RSpec.configure do |config|
+  source_environment_file!
+
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
 
   config.filter_run :focus
+  config.filter_run_excluding type: :feature unless ENV['S3_BUCKET_NAME']
   config.run_all_when_everything_filtered = true
   config.order = :random
 
@@ -25,5 +28,17 @@ RSpec.configure do |config|
 
   Kernel.srand config.seed
 
-  source_environment_file!
+  config.before(:all, type: :feature) do
+    CarrierWave.configure do |config|
+      config.storage    = :aws
+      config.aws_bucket = ENV['S3_BUCKET_NAME']
+      config.aws_acl    = :public_read
+
+      config.aws_credentials = {
+        access_key_id:     ENV['S3_ACCESS_KEY'],
+        secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+        region:            ENV['S3_REGION']
+      }
+    end
+  end
 end
