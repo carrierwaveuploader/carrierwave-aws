1cb1d8d5f8d39339d161b6f4061f80a858efd79d
Parker Selbert <parker@sorentwo.com>
2015-05-31 22:05:05 +0200

    Favor the standard AWS_REGION key over S3_REGION

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,7 +37,7 @@ RSpec.configure do |config|
       config.aws_credentials = {
         access_key_id:     ENV['S3_ACCESS_KEY'],
         secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-        region:            ENV['S3_REGION']
+        region:            ENV['AWS_REGION']
       }
     end
   end
