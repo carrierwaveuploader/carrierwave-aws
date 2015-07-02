67cc7eb2c7359534bfe57af5c5073060271ecc3a
Parker Selbert <parker@sorentwo.com>
2015-07-02 13:32:48 -0500

    Validate aws_acl during configuration time
    The acl is normalized during configuration to prevent upgrade issues and
    make runtime errors less common. For example, `:'public_read'` will be
    automatically convered to `'public-read'`. Attemtping to set an invalid
    configuration value will raise an immediate `ConfigurationError`.

--- lib/carrierwave-aws.rb
+++ lib/carrierwave-aws.rb
@@ -6,15 +6,51 @@ require 'carrierwave/storage/aws_options'
 require 'carrierwave/support/uri_filename'
 
 class CarrierWave::Uploader::Base
+  ACCEPTED_ACL = %w[
+    private
+    public-read
+    public-read-write
+    authenticated-read
+    bucket-owner-read
+    bucket-owner-full-control
+  ].freeze
+
+  ConfigurationError = Class.new(StandardError)
+
   add_config :aws_attributes
   add_config :aws_authenticated_url_expiration
   add_config :aws_credentials
   add_config :aws_bucket
-  add_config :aws_acl
   add_config :aws_read_options
   add_config :aws_write_options
 
   configure do |config|
     config.storage_engines[:aws] = 'CarrierWave::Storage::AWS'
+  end
+
+  def self.aws_acl
+    @aws_acl
+  end
+
+  def self.aws_acl=(acl)
+    @aws_acl = normalized_acl(acl)
+  end
+
+  def self.normalized_acl(acl)
+    normalized = acl.to_s.downcase.sub('_', '-')
+
+    unless ACCEPTED_ACL.include?(normalized)
+      raise ConfigurationError.new("Invalid ACL option: #{normalized}")
+    end
+
+    normalized
+  end
+
+  def aws_acl
+    @aws_acl || self.class.aws_acl
+  end
+
+  def aws_acl=(acl)
+    @aws_acl = self.class.normalized_acl(acl)
   end
 end
--- spec/carrierwave-aws_spec.rb
+++ spec/carrierwave-aws_spec.rb
@@ -1,13 +1,49 @@
 require 'spec_helper'
 
 describe CarrierWave::Uploader::Base do
-  it 'defines aws specific storage options' do
-    expect(described_class).to respond_to(:aws_attributes)
-  end
+  let(:uploader) { CarrierWave::Uploader::Base }
 
   it 'inserts aws as a known storage engine' do
-    described_class.configure do |config|
+    uploader.configure do |config|
       expect(config.storage_engines).to have_key(:aws)
+    end
+  end
+
+  it 'defines aws specific storage options' do
+    expect(uploader).to respond_to(:aws_attributes)
+  end
+
+  describe '#aws_acl' do
+    it 'allows known acess control values' do
+      expect {
+        uploader.aws_acl = 'private'
+        uploader.aws_acl = 'public-read'
+        uploader.aws_acl = 'authenticated-read'
+      }.not_to raise_exception
+    end
+
+    it 'does not allow unknown control values' do
+      expect {
+        uploader.aws_acl = 'everybody'
+      }.to raise_exception
+    end
+
+    it 'normalizes the set value' do
+      uploader.aws_acl = :'public-read'
+      expect(uploader.aws_acl).to eq('public-read')
+
+      uploader.aws_acl = 'PUBLIC_READ'
+      expect(uploader.aws_acl).to eq('public-read')
+    end
+
+    it 'can be overridden on an instance level' do
+      instance = uploader.new
+
+      uploader.aws_acl = 'private'
+      instance.aws_acl = 'public-read'
+
+      expect(uploader.aws_acl).to eq('private')
+      expect(instance.aws_acl).to eq('public-read')
     end
   end
 end
